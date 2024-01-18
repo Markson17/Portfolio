158404d1278eb1083cc18a117ff27b9605b0ad20
Favour Markson <95889759+Markson17@users.noreply.github.com>
2024-01-18 22:43:08 +0000

    modified the home, about and porfolio pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Software Engineer with extensive expertise in server-side development and database design. My proficiency lies in backend development, with a specialization in utilizing Node.js and Django frameworks.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Backend development",
+  "Database design",
+  "API development",
+  "Node.js",
+  "Django",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My track record includes exemplary team building and management skills, with a demonstrated history of success. I am confident in my ability to excel within collaborative atmospheres, aligning seamlessly with company strengths.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Learning Log",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Learning Log is a web application that is used to allow users to create topics they want to learn about, and then add entries to each of those topics as they learn more about them.",
+    url: "https://learning-log-ng7f.onrender.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Story Books",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "The StoryBook App is a web application that empowers users to create, explore, and share captivating stories. It was built with NodeJs, Express and MongoDB. I also integrated the Google and Microsoft Auth for Sign in.",
+    url: "https://story-book-app-markson.onrender.com",
   },
   {
-    title: "My Resume Site",
+    title: "Huddle Landing Page",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is a Huddle sample landing page I redesigned for practice, a community-building platform. It includes information about Huddle's features and benefits",
+    url: "https://markson17.github.io/Huddle-webpage",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Jobs API",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "The Jobs API provides endpoints for efficient management of job listings. With features like user registration, authentication, and CRUD operations on job postings. Built on Node.js, Express, and MongoDB, it offers user-friendly documentation through Swagger UI, catering to job seekers.",
+    url: "https://github.com/Markson17/Jobs-API",
   },
 ];
 
