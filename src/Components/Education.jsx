af8ab826cac15e0e152eb72bd87e04ac6706f368
Favour Markson <95889759+Markson17@users.noreply.github.com>
2024-01-19 00:45:43 +0000

    updated the styles and added the Education section.

---
+++ src/Components/Education.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="dark" id="education">
+            <h2>Education</h2>
+            <div className="content">
+                <div className="content__item">
+                    <h3>University of Ibadan</h3>
+                    <p>2022 - 2026</p>
+                    <p>Bachelor of Science (BSc), Computer Science</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Education;
